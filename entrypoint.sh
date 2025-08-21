6737adbc952bd17c9c188c03ad8a66a072eeabf0
José Javier Quesada Madrigal <xocechka@gmail.com>
2025-08-21 16:15:29 -0400

    chore: update entrypoint.sh to conditionally run server based on environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,11 @@ python3 manage.py createcachetable;
 # Static files
 python3 manage.py collectstatic --noinput;
 
-python3 manage.py runserver 0.0.0.0:8000;
+# Check environment and run appropriate command
+if [ "$DJANGO_DEBUG" = "True" ]; then
+    # For development environment
+    python3 manage.py runserver 0.0.0.0:8000 --settings config.settings
+else
+    # For production environment
+    python3 -m gunicorn -c config/gunicorn_config.py config.wsgi:application;
+fi
